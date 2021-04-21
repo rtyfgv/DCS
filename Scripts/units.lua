bdb64592f91e42a60cd649cb865c30cc1dae3dd2
rtyfgv <61385137+rtyfgv@users.noreply.github.com>
2021-04-21 20:38:10 -0300

    Add files via upload

---
+++ Scripts/units.lua
@@ -0,0 +1,388 @@
+unitKeys = {
+	"alt_type",
+	"unitId",
+	"alt",
+	"psi",
+	"y",
+	"x",
+	"name",
+	"heading",
+	"speed",
+	"skill",
+}
+
+myunits = {
+	["FW-190A8"] = {
+		["hardpoint_racks"] = false,
+		["livery_id"] = "FW-190A8_2.JG 54_Hans Dortenmann",
+		["AddPropAircraft"] = 
+		{
+			["FW_MW50TankContents"] = 0,
+		}, -- end of ["AddPropAircraft"]
+		["type"] = "FW-190A8",
+		["payload"] = 
+		{
+			["pylons"] = 
+			{
+			}, -- end of ["pylons"]
+			["fuel"] = 245,
+			["flare"] = 0,
+			["ammo_type"] = 1,
+			["chaff"] = 0,
+			["gun"] = 100,
+		}, -- end of ["payload"]
+		["callsign"] = 
+		{
+			[1] = 2,
+			[2] = 1,
+			[3] = 1,
+			["name"] = "Springfield11",
+		}, -- end of ["callsign"]
+		["onboard_num"] = "011",
+	},
+	["P-51D"] = {
+		["livery_id"] = "Germany Training Staffel",
+		["type"] = "P-51D",
+		["Radio"] = 
+		{
+			[1] = 
+			{
+				["modulations"] = 
+				{
+				}, -- end of ["modulations"]
+				["channels"] = 
+				{
+					[1] = 124,
+					[2] = 124,
+					[4] = 139,
+					[3] = 131,
+				}, -- end of ["channels"]
+			}, -- end of [1]
+		}, -- end of ["Radio"]
+		["payload"] = 
+		{
+			["pylons"] = 
+			{
+			}, -- end of ["pylons"]
+			["fuel"] = 220,
+			["flare"] = 0,
+			["chaff"] = 0,
+			["gun"] = 100,
+		}, -- end of ["payload"]
+		["callsign"] = 
+		{
+			[1] = 1,
+			[2] = 1,
+			[3] = 1,
+			["name"] = "Enfield11",
+		}, -- end of ["callsign"]
+		["onboard_num"] = "010",
+	},
+	["FW-190D9"] = {
+		["livery_id"] = "FW-190D9_Black 4 of Stab IIJG 6",
+		["AddPropAircraft"] = 
+		{
+			["FW_MW50TankContents"] = 1,
+		}, -- end of ["AddPropAircraft"]
+		["type"] = "FW-190D9",
+		["Radio"] = 
+		{
+			[1] = 
+			{
+				["modulations"] = 
+				{
+				}, -- end of ["modulations"]
+				["channels"] = 
+				{
+					[2] = 38.4,
+					[3] = 41,
+					[1] = 39,
+					[4] = 42,
+					[5] = 38,
+				}, -- end of ["channels"]
+			}, -- end of [1]
+		}, -- end of ["Radio"]
+		["payload"] = 
+		{
+			["pylons"] = 
+			{
+			}, -- end of ["pylons"]
+			["fuel"] = 299,
+			["flare"] = 0,
+			["ammo_type"] = 2,
+			["chaff"] = 0,
+			["gun"] = 100,
+		}, -- end of ["payload"]
+		["callsign"] = 
+		{
+			[1] = 1,
+			[2] = 1,
+			[3] = 1,
+			["name"] = "Enfield11",
+		}, -- end of ["callsign"]
+		["onboard_num"] = "013",
+	},
+	["Bf-109K-4"] = {
+		["livery_id"] = "Bf-109 K4 Jagdgeschwader 53",
+		["AddPropAircraft"] = 
+		{
+			["MW50TankContents"] = 1,
+			["Flare_Gun"] = 1,
+		}, -- end of ["AddPropAircraft"]
+		["type"] = "Bf-109K-4",
+		["Radio"] = 
+		{
+			[1] = 
+			{
+				["modulations"] = 
+				{
+				}, -- end of ["modulations"]
+				["channels"] = 
+				{
+					[2] = 40,
+					[3] = 41,
+					[1] = 39,
+					[4] = 42,
+					[5] = 38,
+				}, -- end of ["channels"]
+			}, -- end of [1]
+		}, -- end of ["Radio"]
+		["payload"] = 
+		{
+			["pylons"] = 
+			{
+			}, -- end of ["pylons"]
+			["fuel"] = 228,
+			["flare"] = 0,
+			["ammo_type"] = 2,
+			["chaff"] = 0,
+			["gun"] = 100,
+		}, -- end of ["payload"]
+		["callsign"] = 
+		{
+			[1] = 1,
+			[2] = 1,
+			[3] = 1,
+			["name"] = "Enfield11",
+		}, -- end of ["callsign"]
+		["onboard_num"] = "013",
+	}, -- end of [1]
+	["SpitfireLFMkIXCW"] = {
+		["type"] = "SpitfireLFMkIXCW",
+		["Radio"] = 
+		{
+			[1] = 
+			{
+				["modulations"] = 
+				{
+				}, -- end of ["modulations"]
+				["channels"] = 
+				{
+					[2] = 40,
+					[3] = 41,
+					[1] = 124,
+					[4] = 42,
+					[5] = 108.9,
+				}, -- end of ["channels"]
+			}, -- end of [1]
+		}, -- end of ["Radio"]
+		["payload"] = 
+		{
+			["pylons"] = 
+			{
+			}, -- end of ["pylons"]
+			["fuel"] = 220,
+			["flare"] = 0,
+			["chaff"] = 0,
+			["gun"] = 100,
+		}, -- end of ["payload"]
+		["callsign"] = 
+		{
+			[1] = 1,
+			[2] = 1,
+			[3] = 1,
+			["name"] = "Enfield11",
+		}, -- end of ["callsign"]
+		["onboard_num"] = "010",
+	},
+	["SpitfireLFMkIX"] = {
+		["type"] = "SpitfireLFMkIX",
+		["Radio"] = 
+		{
+			[1] = 
+			{
+				["modulations"] = 
+				{
+				}, -- end of ["modulations"]
+				["channels"] = 
+				{
+					[2] = 40,
+					[3] = 41,
+					[1] = 124,
+					[4] = 42,
+					[5] = 108.9,
+				}, -- end of ["channels"]
+			}, -- end of [1]
+		}, -- end of ["Radio"]
+		["payload"] = 
+		{
+			["pylons"] = 
+			{
+			}, -- end of ["pylons"]
+			["fuel"] = 220,
+			["flare"] = 0,
+			["chaff"] = 0,
+			["gun"] = 100,
+		}, -- end of ["payload"]
+		["callsign"] = 
+		{
+			[1] = 1,
+			[2] = 1,
+			[3] = 1,
+			["name"] = "Enfield11",
+		}, -- end of ["callsign"]
+		["onboard_num"] = "010",
+	},
+	["P-47D-30"] = {
+		["hardpoint_racks"] = false,
+		["livery_id"] = "1st Brazilian Ftr Sq-Jambock A1-Menezes",
+		["AddPropAircraft"] = 
+		{
+			["WaterTankContents"] = 1,
+		}, -- end of ["AddPropAircraft"]
+		["type"] = "P-47D-30",
+		["Radio"] = 
+		{
+			[1] = 
+			{
+				["modulations"] = 
+				{
+				}, -- end of ["modulations"]
+				["channels"] = 
+				{
+					[1] = 124,
+					[2] = 40,
+					[4] = 42,
+					[3] = 41,
+				}, -- end of ["channels"]
+			}, -- end of [1]
+			[2] = 
+			{
+				["channels"] = 
+				{
+					[1] = 108.9,
+				}, -- end of ["channels"]
+			}, -- end of [2]
+		}, -- end of ["Radio"]
+		["payload"] = 
+		{
+			["pylons"] = 
+			{
+			}, -- end of ["pylons"]
+			["fuel"] = 272,
+			["flare"] = 0,
+			["ammo_type"] = 1,
+			["chaff"] = 0,
+			["gun"] = 100,
+		}, -- end of ["payload"]
+		["callsign"] = 
+		{
+			[1] = 4,
+			[2] = 1,
+			[3] = 1,
+			["name"] = "Colt11",
+		}, -- end of ["callsign"]
+		["onboard_num"] = "013",
+	},
+	["I-16"] = {
+		["livery_id"] = "Red Army Camo",
+		["AddPropAircraft"] = 
+		{
+			["landingTorch"] = false,
+		}, -- end of ["AddPropAircraft"]
+		["type"] = "I-16",
+		["Radio"] = 
+		{
+			[1] = 
+			{
+				["modulations"] = 
+				{
+				}, -- end of ["modulations"]
+				["channels"] = 
+				{
+					[1] = 124,
+					[2] = 124,
+					[4] = 139,
+					[3] = 131,
+				}, -- end of ["channels"]
+			}, -- end of [1]
+		}, -- end of ["Radio"]
+		["payload"] = 
+		{
+			["pylons"] = 
+			{
+			}, -- end of ["pylons"]
+			["fuel"] = 100,
+			["flare"] = 0,
+			["chaff"] = 0,
+			["gun"] = 100,
+		}, -- end of ["payload"]
+		["callsign"] = 101,
+		["onboard_num"] = "010",
+	}
+}
+
+function printTable (arg)
+	for k,v in pairs(arg) do
+		if type(v) ~= 'table' then
+			print (k .. " -> " .. v)
+		end
+	end
+end
+
+function copy(dest, src, keys)
+	if keys == nil then
+		for k,v in pairs(src) do
+			dest[k] = v
+		end
+	else
+		for k,v in pairs(keys) do
+			dest[v] = src[v]
+		end
+	end
+	return dest;
+end
+
+function addSubmenu (menu, subMenu, callback)
+	for k,v in pairs(subMenu) do
+		missionCommands.addCommandForCoalition(coalition.side.BLUE, v, menu, callback, k) 
+	end
+end
+
+do 
+	function spawnCallback(level)
+		trigger.action.setUserFlag('1', level)
+	end
+
+	function destroyCallback(level)
+		trigger.action.setUserFlag('1', level)
+	end
+
+	spawnMenu   = missionCommands.addCommandForCoalition(coalition.side.BLUE, 'Spawn',   nil, spawnCallback, 1)
+	destroyMenu = missionCommands.addCommandForCoalition(coalition.side.BLUE, 'Destroy', nil, destroyCallback, 1)
+	planeMenu   = missionCommands.addSubMenuForCoalition(coalition.side.BLUE, 'Plane',   nil)
+	skillMenu   = missionCommands.addSubMenuForCoalition(coalition.side.BLUE, 'Skill',   nil)
+	skillLevels = { 'Excellent', 'High', 'Good', 'Average', }
+	planes      = {	'Bf-109K-4', 'FW-190A8', 'FW-190D9', 'I-16', 'P-47D-30', 'P-51D', 'SpitfireLFMkIX',	'SpitfireLFMkIXCW',}
+		
+	function skillCallback(level)
+		trigger.action.setUserFlag('1', level)
+	end
+	addSubmenu(skillMenu, skillLevels, skillCallback)
+
+	function planeCallback(plane)
+		trigger.action.setUserFlag('2', plane)
+	end
+	addSubmenu(planeMenu, planes, planeCallback)
+end
+
